30f3f0f99e9fe79a37987d71542cf7689b4a4111
zyinghua <49605250+zyinghua@users.noreply.github.com>
2026-09-24 21:26:16 -0400

    Update arXiv links and citation

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 // Add release URLs here. The resource links become active automatically.
 window.ROLLING_WAM_LINKS = {
-  paper: "",
+  paper: "https://arxiv.org/abs/2609.30247",
   code: "https://github.com/zyinghua/Rolling-WAM"
 };
